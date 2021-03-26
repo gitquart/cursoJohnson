ba66505f858f81ac70a9efd95e84b153332cdade
Ulyses <ulyses.rico.2012@gmail.com>
2021-03-26 14:46:35 -0600

    ...

--- codigo_M.m
+++ codigo_M.m
@@ -8,7 +8,7 @@ de sintaxis
 */
 
 Let
-    Source = Excel.Workbook(File.Contents("C:\Users\1098350515\Documents\CursoJohnson\DB_Curso.xlsx"), null, true),
+    Source = Excel.Workbook(File.Contents("C:\Users\1098350515\Documents\quart\cursoJohnson\DB_Curso.xlsx"), null, true),
     #"Tabla 1_Sheet" = Source{[Item="Tabla 1",Kind="Sheet"]}[Data],
     #"Promoted Headers" = Table.PromoteHeaders(#"Tabla 1_Sheet", [PromoteAllScalars=true]),
     #"Changed Type" = Table.TransformColumnTypes(#"Promoted Headers",{{"ID", Int64.Type}, {"Nombre ", type text}, {"Edad", Int64.Type}, {"Ciudad", type text}}),
