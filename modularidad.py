9d13923327f9b3d50a6f3ef1cc1233e990f9eaf2
Ulyses <ulyses.rico.2012@gmail.com>
2021-03-26 11:58:20 -0600

    ...

---
+++ modularidad.py
@@ -0,0 +1,14 @@
+"""
+This example aims to show MODULARITY in python
+"""
+
+
+print('Hola, soy un robot que te ayudará a presentarte...')
+print('¿Cuál es tu nombre?\n')
+nombre=input()
+print('¿Qué te gusta hacer?')
+actividad=input()
+presentacion='Hola, me llamo '+nombre+' y me gusta '+actividad+'. Estoy programando en el curso\n'
+print('*****************************************')
+print(presentacion)
+print('****************************************')
