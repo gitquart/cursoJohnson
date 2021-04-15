47947921030cbeb667a8974822648abcbe27c841
Ulyses <ulyses.rico.2012@gmail.com>
2021-04-15 15:03:51 -0500

    ...

--- data_base_escuela.sql
+++ data_base_escuela.sql
@@ -107,5 +107,13 @@ BEGIN
 END
 CLOSE ProdInfo
 DEALLOCATE ProdInfo
-	
+
+/*Example of CASE*/
+
+SELECT OrderID, Quantity,
+CASE WHEN Quantity > 30 THEN 'The quantity is greater than 30'
+WHEN Quantity = 30 THEN 'The quantity is 30'
+ELSE 'The quantity is under 30'
+END AS QuantityText
+FROM OrderDetails;	
 
